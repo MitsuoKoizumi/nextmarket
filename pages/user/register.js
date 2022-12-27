f8c5ac3866d66b304b61bcad711b0762a39f0e57
MitsuoKoizumi <m_koizumi@yumemi.co.jp>
2022-12-27 21:55:49 +0900

    コメント削除

--- pages/user/register.js
+++ pages/user/register.js
@@ -19,7 +19,7 @@ const Register = () => {
     }
 
     const handleSubmit = async (event) => {
-        event.preventDefault() //submit時に画面をリロードさせない
+        event.preventDefault() 
         try{
             //fetchはデータの送信にも使える
             const response = await fetch('http://localhost:3000/api/user/register',{
